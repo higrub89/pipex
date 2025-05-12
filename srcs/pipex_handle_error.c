5d6a5b802dd56419640cfc7348c6ed5b6fdbe10e
rhiguita <higuitaruben@hotmail.com>
2025-05-12 22:51:55 +0200

    update

--- srcs/pipex_handle_error.c
+++ srcs/pipex_handle_error.c
@@ -2,7 +2,7 @@
 
 void ft_is_empty(char *cmd)
 {
-  while ((*cmd >= 9 && *cmd <= 13) || *cm == ' ')
+  while ((*cmd >= 9 && *cmd <= 13) || *cmd == ' ')
     cmd++;
   if (*cmd == '\0')
     (ft_command_error(cmd));
