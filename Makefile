cd27cbf990914f4b8fc56868ef73b5018585de25
rhiguita <higuitaruben@hotmail.com>
2025-05-14 22:50:54 +0200

    update .h

--- Makefile
+++ Makefile
@@ -25,7 +25,9 @@ CFLAGS				= -Wall -Werror -Wextra -g $(INC)
 RM					= rm -f
 
 # Source Files
-COMMANDS_DIR		=	$(SRC_DIR) 
+COMMANDS_DIR		=	$(SRC_DIR)	pipex.c \
+															pipex_handle_error.c \
+															pipex_utils.c 
 
 # Concatenate all source files
 SRCS 				= $(COMMANDS_DIR) 
--- inc/pipex.h
+++ inc/pipex.h
@@ -22,5 +22,12 @@
 # include <sys/stat.h>
 # include <sys/wait.h>
 
-
+int		exec_command(char **full_cmd, char **env);
+void	ft_check_command(char *cmd, char **envp);
+char	*ft_find_path(char **envp);
+void	ft_free_memory(char **tab);
+char	*ft_get_path(char **full_cmd, char **envp);
+void	ft_error_file(int *file_pipe, char *file);
+int		ft_command_error(char *cmd);
+void	ft_is_empty(char *cmd);
 #endif
