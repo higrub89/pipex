4a6073b451f8f870bd476945ebe36857a2d3e820
rhiguita <higuitaruben@hotmail.com>
2025-05-15 15:03:57 +0200

    creation files test

--- libft/src/ft_str/ft_strjoin.c
+++ libft/src/ft_str/ft_strjoin.c
@@ -17,11 +17,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*res;
 	size_t	buffer;
 
+	if (!s1 || !s2)
+		return (NULL);
 	buffer = ft_strlen(s1) + ft_strlen(s2);
 	res = (char *)malloc(sizeof(char) * (buffer + 1));
 	if (!res)
 		return (NULL);
-	ft_strlcpy(res, s1, ft_strlen(s1));
+	ft_strlcpy(res, s1, buffer + 1);
 	ft_strlcat(res, s2, buffer + 1);
 	return (res);
 }
